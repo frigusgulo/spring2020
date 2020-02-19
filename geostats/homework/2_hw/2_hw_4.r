cf3a8cac7ee32226b4f59be438b5a3b99af42456
frigusgulo <franklyn.dunbar@umontana.edu>
2020-02-18 19:08:00 -0700

    .

--- geostats/homework/2_hw/2_hw_4.r
+++ geostats/homework/2_hw/2_hw_4.r
@@ -1,6 +1,6 @@
 library(rgdal)
 library(sp)
-
+library(splancs)
 gravity = read.table("~/spring2020/geostats/homework/2_hw/gravity255.txt",header=T,sep=",")
 source("~/spring2020/geostats/scripts/point.crossval.r")
 source("~/spring2020/geostats/scripts/LongLatToUTM.r")
@@ -13,6 +13,9 @@ xy <- LongLatToUTM(gravity$long, gravity$lat,11)
 p <- 2
 r <- c(20000,40000,60000,80000,100000)
 
+x = xy$X
+y = xy$Y
+z = gravity$gravity
 for (i in 1:5){
   point.crossval(xy$X,xy$Y,gravity$gravity,2,r[i])
 }
@@ -24,24 +27,24 @@ for (i in 1:length(p_)) {
 
 
 #QQ plots
-out <- point.crossval(x,y,z,0.5, 30000)
+out <- point.crossval(xy$X,xy$Y,gravity$gravity,1.0,40000)
+qqplot( gravity$gravity,out$pred.pd)
+qqplot( gravity$gravity,out$pred.tr)
+qqplot( gravity$gravity,out$pred.ls)
+qqplot( gravity$gravity,out$pred.iw)
 
-plot(sort(out$pred.pd), sort(gravity$gravity))
-plot(sort(out$pred.tr), sort(gravity$gravity))
-plot(sort(out$pred.ls), sort(gravity$gravity))
-plot(sort(out$pred.is), sort(gravity$gravity))
+#scatters
+plot(gravity$gravity,out$pred.pd,main="Prediction Polygonal Declustering")
+lines(lowess( gravity$gravity,out$pred.pd))
+
+plot( gravity$gravity,out$pred.tr,main="Prediction Triangulation")
+lines(lowess( gravity$gravity,out$pred.tr))
+
+plot( gravity$gravity,out$pred.ls,main="Prediction Local Sample Mean")
+lines(lowess(gravity$gravity,out$pred.ls))
+
+plot(gravity$gravity,out$pred.iw,main="Prediction Inverse Distance")
+lines(lowess(gravity$gravity,out$pred.iw))
 
 
 
-# f
-# polydec
-pd = polydec(snow$x,snow$y,peel=7)
-pd = pd / sum(pd)
-pd_mean = sum(snow$depth*pd)
-
-#celldec
-celldec(snow$x,snow$y,snow$depth,i,j)
-
-#idw
-
-#triangulation
